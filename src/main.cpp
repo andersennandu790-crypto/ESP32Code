83f573ac8964958bb8feeed5fd07094b18840899
esmee <1251464233@qq.com>
2026-06-06 15:06:13 +0800

    完成串口控制LED作业

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,37 @@
 #include <Arduino.h>
 
-int ledPins[] = {13, 12, 14, 27, 26, 25};
-int ledCount = 6;
-int delayTime = 300;
+int ledPins[] = {13, 12, 14};
+bool ledStates[] = {false, false, false};
+int ledCount = 3;
 
 void setup() {
+  Serial.begin(115200);
+
   for (int i = 0; i < ledCount; i++) {
     pinMode(ledPins[i], OUTPUT);
+    digitalWrite(ledPins[i], LOW);
   }
+
+  Serial.println("Serial LED Control Ready");
+  Serial.println("Send 1, 2, or 3 to toggle LEDs");
 }
 
 void loop() {
-  for (int i = 0; i < ledCount; i++) {
-    digitalWrite(ledPins[i], HIGH);
-    delay(delayTime);
-    digitalWrite(ledPins[i], LOW);
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+
+    if (command >= '1' && command <= '3') {
+      int index = command - '1';
+
+      ledStates[index] = !ledStates[index];
+      digitalWrite(ledPins[index], ledStates[index] ? HIGH : LOW);
+
+      Serial.print("LED");
+      Serial.print(index + 1);
+      Serial.print(" ");
+      Serial.println(ledStates[index] ? "ON" : "OFF");
+    } else if (command != '\n' && command != '\r') {
+      Serial.println("Unknown command");
+    }
   }
 }
